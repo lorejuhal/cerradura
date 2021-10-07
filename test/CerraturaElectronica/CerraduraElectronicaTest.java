912fdb334b5a34dae4682aed469951a9cdd861c4
LoreJuhal <ljuhal@alumno.unlam.edu.ar>
2021-10-07 17:10:11 -0300

    se creo proyecto

---
+++ test/CerraturaElectronica/CerraduraElectronicaTest.java
@@ -0,0 +1,15 @@
+package CerraturaElectronica;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CerraduraElectronicaTest {
+
+	@Test
+	public void AlcrearLaCerraduraElectronicaEstaAbierta() {
+		CerraduraElectronica cerradura = new CerraduraElectronica();
+		assertTrue(cerradura.estaAbierta());
+			}
+
+}
